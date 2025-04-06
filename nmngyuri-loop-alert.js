b9cc12e2003d65b676cde0fc0e658c62f2c0a96f
さきちび <sakitibi10nin@icloud.com>
2025-04-06 22:21:31 +0900

    Update nmngyuri-loop-alert.js

--- nmngyuri-loop-alert.js
+++ nmngyuri-loop-alert.js
@@ -1,12 +1,19 @@
 let nmngyuri = Math.floor(Math.random() * 2);
+let count = 0;
 
-$(function(){
-    setInterval(function(){
-      nmngyuri = Math.floor(Math.random() * 2);
-      if (nmngyuri === 0){
-          alert("名前は長い方が有利戦争の応戦にご協力をお願い致します。");
-      } else if (nmngyuri === 1) {
-          alert("名前は長い方が有利反対にご協力をお願い致します。");
-      }
-    },1000);
-});
+while( count >= 0){
+    count + 1;
+    if (count === 1000){
+        nmngyurialert();
+    }
+}
+
+function nmngyurialert(){
+    nmngyuri = Math.floor(Math.random() * 2);
+    if (nmngyuri === 0){
+        alert("名前は長い方が有利戦争の応戦にご協力をお願い致します。");
+    } else if (nmngyuri === 1) {
+        alert("名前は長い方が有利反対にご協力をお願い致します。");
+    }
+    count = 0;
+}
