1170cfe6c48537407ad75be2dc46a88aa3b48951
さきちび <sakitibi10nin@icloud.com>
2025-04-06 22:03:46 +0900

    Create nmngyuri-loop-alert.js

---
+++ nmngyuri-loop-alert.js
@@ -0,0 +1,12 @@
+let nmngyuri = Math.floor(Math.random() * 2);
+
+$(function(){
+    setInterval(function(){
+      nmngyuri = Math.floor(Math.random() * 2);
+      if (nmngyuri === 0){
+          alert("名前は長い方が有利戦争の応戦にご協力をお願い致します。");
+      } else if (nmngyuri === 1) {
+          alert("名前は長い方が有利反対にご協力をお願い致します。");
+      }
+    },100000);
+});
