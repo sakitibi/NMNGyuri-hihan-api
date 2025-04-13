ba224ea693ce10b16e66c6fe8c5076706e4a9973
さきちび <sakitibi10nin@icloud.com>
2025-04-13 12:23:46 +0900

    Create nmngyuri-popup.js

---
+++ nmngyuri-popup.js
@@ -0,0 +1,9 @@
+let nmngyuripopup = Math.floor(Math.random() * 1);
+
+if (nmngyuri === 0){
+    popup1();
+}
+
+function popup1(){
+  window.open("https://sakitibi-com9.webnode.jp/page/25", "popupWindow", "width=800,height=600");
+}
