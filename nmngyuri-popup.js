60f55404715a2859398460db613e1e1ae04e8e73
さきちび <sakitibi10nin@icloud.com>
2025-04-13 12:49:38 +0900

    Update nmngyuri-popup.js

--- nmngyuri-popup.js
+++ nmngyuri-popup.js
@@ -1,15 +1,16 @@
-let nmngyuripopup = Math.floor(Math.random() * 2);
+let nmngyuripopup = Math.floor(Math.random() * 5);
 
-if (nmngyuripopup === 0){
-    popup1();
-} else if (nmngyuripopup === 1) {
-    popup2();
-}
-
-function popup1(){
-  window.open("https://sakitibi-com9.webnode.jp/page/25", "popupWindow", "width=800,height=600");
-}
-
-function popup2(){
-  window.open("https://sakitibi-com9.webnode.jp/page/25-2", "popupWindow", "width=800,height=600");
+popup();
+function popup(){
+    if (nmngyuripopup === 0){
+        window.open("https://sakitibi-com9.webnode.jp/page/25", "popupWindow", "width=800,height=600");
+    } else if (nmngyuripopup === 1) {
+        window.open("https://sakitibi-com9.webnode.jp/page/25-2", "popupWindow", "width=800,height=600");
+    } else if (nmngyuripopup === 2) {
+        window.open("https://sakitibi-com9.webnode.jp/page/25-3", "popupWindow", "width=800,height=600");
+    } else if (nmngyuripopup === 3) {
+        window.open("https://sakitibi-com9.webnode.jp/page/25-4", "popupWindow", "width=800,height=600");
+    } else if (nmngyuripopup === 4) {
+        window.open("https://sakitibi-com9.webnode.jp/page/25/comment-list", "popupWindow", "width=800,height=600");
+    }
 }
