9bff283d32ed3eadaf2c790b9004fd330155e93d
さきちび <sakitibi10nin@icloud.com>
2025-04-20 15:10:28 +0900

    Update nmngyuri-loop-alert.js

--- nmngyuri-loop-alert.js
+++ nmngyuri-loop-alert.js
@@ -1,19 +1,19 @@
 let nmngyuriloopalert = Math.floor(Math.random() * 3);
-  if (nmngyurialert === 0){
+  if (nmngyuriloopalert === 0){
     alert("名前は長い方が有利戦争の応戦にご協力をお願い致します。");
-  } else if (nmngyurialert === 1) {
+  } else if (nmngyuriloopalert === 1) {
       alert("名前は長い方が有利反対にご協力をお願い致します。");
-  } else if (nmngyurialert === 2) {
+  } else if (nmngyuriloopalert === 2) {
       alert("名前は長い方が有利は悪質な荒らしです、\n撲滅にご協力をお願い致します");
   }
 
 setInterval(() => {
   nmngyuriloopalert = Math.floor(Math.random() * 3);
-  if (nmngyurialert === 0){
+  if (nmngyuriloopalert === 0){
     alert("名前は長い方が有利戦争の応戦にご協力をお願い致します。");
-  } else if (nmngyurialert === 1) {
+  } else if (nmngyuriloopalert === 1) {
       alert("名前は長い方が有利反対にご協力をお願い致します。");
-  } else if (nmngyurialert === 2) {
+  } else if (nmngyuriloopalert === 2) {
       alert("名前は長い方が有利は悪質な荒らしです、\n撲滅にご協力をお願い致します");
   }
 }, 150000);
