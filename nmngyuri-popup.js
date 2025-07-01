691533321df558d37bb0a285317a81c89300c241
さきちび <sakitibi10nin@icloud.com>
2025-07-01 22:51:20 +0900

    Update nmngyuri-popup.js

--- nmngyuri-popup.js
+++ nmngyuri-popup.js
@@ -1,4 +1,4 @@
-let nmngyuripopup = Math.floor(Math.random() * 5);
+let nmngyuripopup = Math.floor(Math.random() * 6);
 
 popup();
 function popup(){
@@ -12,5 +12,7 @@ function popup(){
         window.open("https://sakitibi-com9.webnode.jp/page/25-4", "popupWindow", "width=800,height=600");
     } else if (nmngyuripopup === 4) {
         window.open("https://sakitibi-com9.webnode.jp/page/25/comment-list", "popupWindow", "width=800,height=600");
+    } else if (nmngyuripopup === 5){
+        window.open("https://sakitibi-com9.webnode.jp/page/25-5", "popupWindow", "width=800,height=600");
     }
 }
