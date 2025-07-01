51b93d50cf5ded72a352e14eba1bd29d9ce5529d
さきちび <sakitibi10nin@icloud.com>
2025-07-01 22:55:04 +0900

    Update nmngyuri-alert.js

--- nmngyuri-alert.js
+++ nmngyuri-alert.js
@@ -1,4 +1,4 @@
-let nmngyurialert = Math.floor(Math.random() * 3);
+let nmngyurialert = Math.floor(Math.random() * 4);
 
 if (nmngyurialert === 0){
     alert("名前は長い方が有利戦争の応戦にご協力をお願い致します。");
@@ -6,4 +6,6 @@ if (nmngyurialert === 0){
     alert("名前は長い方が有利反対にご協力をお願い致します。");
 } else if (nmngyurialert === 2) {
     alert("名前は長い方が有利は悪質な荒らしです、\n撲滅にご協力をお願い致します");
+} else if (nmngyurialert === 3){
+    alert("名前は長い方が有利は登録者を買っています\n今すぐ通報して下さい");
 }
