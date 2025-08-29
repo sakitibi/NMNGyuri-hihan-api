f68b02d2c0a55218828a65b8be07518abcecbf44
さきちび <sakitibi10nin@icloud.com>
2025-08-29 09:07:31 +0900

    Update nmngyuri-alert.js

--- nmngyuri-alert.js
+++ nmngyuri-alert.js
@@ -15,16 +15,18 @@ NMNGyuriAlertMain();
 function NMNGyuriAlertMain(){
     const container = document.createElement("div");
     container.classList.add("sms-auth-modal-overlay", "sms-auth-modal-overlay--after-open");
-    document.body.appendChild(container);
     Object.assign(container.style, {
         backgroundColor: "rgba(0, 0, 0, 0.3)",
         zIndex: 999999
     });
+    document.body.appendChild(container);
     container.innerHTML = (`
         <div id="sms-container" style="vertical-align: middle; position: absolute; inset: 50% auto auto 50%; transform: translate(-50%, -50%); z-index: 999999; max-width: 500px; box-sizing: border-box; width: 90%; background: rgb(255, 255, 255); padding: 0px; border: none; border-radius: 8px; box-shadow: rgb(0, 0, 0) 0px 0px 10px; text-align: left;">
             <div class="sms-form">
-                <h3 class="modal-title"></h3>
-                <button class="submit-btn secondary">同意する</button>
+                <h3 class="modal-title">${nmngyuritext}</h3>
+                <div class="button-container">
+                    <button class="submit-btn secondary">同意する</button>
+                </div>
             </div>
         </div>
     `);
@@ -67,6 +69,9 @@ function NMNGyuriAlertMain(){
             margin-bottom: .5rem;
             padding: 4px 10px;
             width: 16rem;
+        }
+        .sms-form .button-container {
+            text-align: center;
         }
     `);
     document.head.appendChild(stylesheet);
